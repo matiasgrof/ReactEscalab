718dc28bcff5dd68e647e456d665bb6847e2188b
Matias Grof <matias.grof@u-payments.com>
2022-06-23 12:09:22 -0400

    NavBar

---
+++ eCommerce/ecommerce/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import ResponsiveAppBar from './components/NavBar.js';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header"> */}
+        <ResponsiveAppBar/>
+      {/* </header> */}
+    </div>
+  );
+}
+
+export default App;
